d9775ba440e87a5cd2d493c1e48b4cd984e360c1
양성수 <112933943+ystgd07@users.noreply.github.com>
2023-10-25 20:01:41 +0900

    Update Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Main() {
-  return <div>Main update i want deploy11 </div>;
+  return <div className="text-bold">Main update i want deploy update </div>;
 }
