aef9a2ae0b1d7ee61b7017353754922c68106e08
iolagvi28 <iolagvi28@gmail.com>
2023-11-02 09:18:26 +0900

    feat: scrollbar

--- src/features/backlog/BacklogHeader.jsx
+++ src/features/backlog/BacklogHeader.jsx
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import CreateButton from '../../ui/CreateButton';
+import TestModal from './TestModal';
 
 export default function BacklogHeader() {
+  function clickBtn() {
+    console.log('[BacklogHeader] clickBtn!');
+  }
+
   return (
     <div className="flex justify-between items-center mx-1">
       <div className="text-3xl text-slate-700 mx-3">백로그</div>
-      <CreateButton value={'이슈 만들기'} />
+      <span onClick={clickBtn()}>
+        <CreateButton value={'이슈 만들기'} onClick={clickBtn} />
+      </span>
     </div>
   );
 }
